2d22604efece08554b563c5827337f0e6888dc16
francisco <0667034@my.scccd.edu>
2018-08-22 14:50:33 -0700

    ceated net pay and  net crew total

--- js/scripts.js
+++ js/scripts.js
@@ -19,12 +19,17 @@ class EmployeePieceWork extends Employee{
     super(employeeId, name, date)
     this._pieceWork = [];
     this._rate = rate;
+    this._netPayArray = [];
+
   }
   get pieceWork(){
     return this._pieceWork;
   }
   get rate(){
     return this._rate;
+  }
+  get netPayArray(){
+    return this._netPayArray;
   }
   // get total(){
   //   return this._total;
@@ -42,10 +47,22 @@ class EmployeePieceWork extends Employee{
 
   netPay(){
     let net = this.getPieceWorkTotal() * this._rate;
+    this._netPayArray.push(net);
     return net;
   }
-
+  // totalNetPay(){
+  //   foreach()
+  // }
 }
+// class Crew extends Employee{
+//   constructor(){
+//   super(netPayArray)
+// }
+// getNetPayTotal(){
+//   let netTotal = this.netPayArray.reduce((a,b) => a + b) 0)
+//   return netTotal;
+// }
+// }
 let george = new EmployeePieceWork('23', 'George', '2456', 45)
 george.addPieceWork(5);
 george.addPieceWork(7);
@@ -54,3 +71,14 @@ console.log(george.getPieceWorkTotal(), 'piece work total');
 console.log(george.netPay(), 'netpay');
 
 console.log('duh');
+let jaun = new EmployeePieceWork('45','jaun', '06/07', 2)
+jaun.addPieceWork(10);
+jaun.addPieceWork(10);
+jaun.addPieceWork(10);
+console.log(jaun.netPay(), 'jauns net pay');
+console.log(jaun.netPayArray, 'array');
+let crew = new Employee();
+console.log(jaun.pieceWork);
+console.log(george.pieceWork);
+console.log(jaun.pieceWork.concat(george.pieceWork).reduce((a,b) => a + b, 0), 'crew piece work total ');
+console.log(jaun.netPayArray.concat(george.netPayArray).reduce((a,b) => a + b, 0), 'crew net pay');
