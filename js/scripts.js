fd256114bcf1fe244bcb28c8d77386ca3bb97475
francisco <0667034@my.scccd.edu>
2018-08-31 00:44:32 -0700

    changed the way i insert the pieces

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,34 @@
+$(document).ready(function($) {
+  "use strict";
+  $.fn.responsiveTable = function() {
+
+    var toggleColumns = function($table) {
+      var selectedControls = [];
+      $table.find(".Accordion, ").each(function() {
+        selectedControls.push($(this).attr("aria-selected"));
+      });
+      var cellCount = 0,
+        colCount = 0;
+      var setNum = $table.find(".Rtable-cell").length / Math.max( $table.find(".Accordion").length);
+      $table.find(".Rtable-cell").each(function() {
+        $(this).addClass("hiddenSmall");
+        if (selectedControls[colCount] === "true") $(this).removeClass("hiddenSmall");
+        cellCount++;
+        if (cellCount % setNum === 0) colCount++;
+      });
+    };
+    $(this).each(function() {
+      toggleColumns($(this));
+    });
+
+
+    $(this).on("click", ".Accordion", function() {
+      $(this).attr("aria-selected", $(this).attr("aria-selected") !== "true");
+      toggleColumns($(this).closest(".Rtable"));
+    });
+
+  };
+
 class Employee {
   constructor(employeeId, name, date) {
     this._employeeId = employeeId;
@@ -32,7 +63,7 @@ class EmployeePieceWork extends Employee {
   get netPayArray() {
     return this._netPayArray;
   }
-  get date(){
+  get date() {
     return this._date;
   }
   // get total(){
@@ -112,20 +143,24 @@ let content = document.getElementById("content-box");
 
 submitEmployee.addEventListener('click', function newEmployeesubmit() {
 
+  var random_id = function  ()
+{
+  var id_num = Math.random().toString(9).substr(2,3);
+  var id_str = Math.random().toString(36).substr(2);
+
+  return id_num + id_str;
+}
+	var row_id = random_id();
   console.log(employee = new EmployeePieceWork(employeeId.value, name.value, '09', rate.value))
   employees.push(employee);
   console.log(employees, 'list of employees');
   let id;
-  let tableRow = document.createElement('tr');
-  let tableCell = document.createElement('td');
   let button = document.createElement('button');
- let p = document.createElement('p');
+  let p = document.createElement('p');
   let employeeIdNow = document.createTextNode(employee.employeeId);
   let employeeNameButton = document.createTextNode(employee.name);
   let employeeRate = document.createTextNode(employee.rate);
   let employeeName = document.createTextNode(employee.name);
-
-
   button.classList = "Accordion";
   button.setAttribute("role", "tab");
   button.setAttribute("aria-selected", "false");
@@ -134,56 +169,156 @@ submitEmployee.addEventListener('click', function newEmployeesubmit() {
   p.appendChild(employeeNameButton);
 
 
+// IMPORTANT DO NOT DELETE
+//   for (let i=0; i<1; i++){
+//
+//     row = document.createElement('div');
+//     row.setAttribute("id", row_id);
+//     row.classList = "Rtable js-RtableAccordions Rtable--collapse  Rtable--12cols ";
+//     content.appendChild(row);
+//         row.appendChild(button);
+//
+//   for (let j = 0; j < 12; j++) {
+//
+//     let div = document.createElement('div');
+//     let colName = ['employeeId', 'name', 'sun', 'mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'piece-total', 'rate', 'nettotal'];
+//     div.setAttribute("edit_type", "click");
+//     div.setAttribute('contenteditable', 'false');
+//     div.setAttribute("name", colName[j]);
+//     div.classList = "Rtable-cell row_data hiddenSmall"
+//     row.append(div);
+//   }
+//
+// }
+
+let idd = document.createElement('div');
+let fName = document.createElement('div');
+let divSun = document.createElement('div');
+let divMon = document.createElement('div');
+let divTues = document.createElement('div');
+let divWed = document.createElement('div');
+let divThur = document.createElement('div');
+let divFri = document.createElement('div');
+let divSat = document.createElement('div');
+let divTotalPieces = document.createElement('div');
+let divRate = document.createElement('div');
+let divNetTotal = document.createElement('div');
+content.appendChild(button);
+content.appendChild(idd);
+ idd.setAttribute("edit_type", "click");
+ idd.setAttribute("class", 'Sun');
+ idd.setAttribute('contenteditable', 'true');
+ idd.classList="Rtable-cell row_data"
+ content.appendChild(fName);
+ fName.setAttribute("edit_type", "click");
+ fName.setAttribute("class", 'Sun');
+ fName.setAttribute('contenteditable', 'true');
+ fName.classList="Rtable-cell row_data"
+  content.appendChild(divSun);
+divSun.setAttribute("edit_type", "click");
+divSun.setAttribute("class", 'Sun');
+divSun.setAttribute('contenteditable', 'true');
+divSun.classList="Rtable-cell row_data"
+divSun.appendChild(employeeRate);
+ content.appendChild(divMon);
+divMon.setAttribute("edit_type", "click");
+divMon.setAttribute("class", 'Mon');
+divMon.setAttribute('contenteditable', 'true');
+divMon.classList="Rtable-cell row_data"
+ content.appendChild(divTues);
+divTues.setAttribute("edit_type", "click");
+divTues.setAttribute("class", 'tues');
+divTues.setAttribute('contenteditable', 'true');
+divTues.classList="Rtable-cell row_data"
+ content.appendChild(divWed);
+divWed.setAttribute("edit_type", "click");
+divWed.setAttribute("class", 'Wed');
+divWed.setAttribute('contenteditable', 'true');
+divWed.classList="Rtable-cell row_data"
+ content.appendChild(divThur);
+divThur.setAttribute("edit_type", "click");
+divThur.setAttribute("name", 'thurs');
+divThur.setAttribute('contenteditable', 'true');
+divThur.classList="Rtable-cell row_data";
+ content.appendChild(divFri);
+divFri.setAttribute("edit_type", "click");
+divFri.setAttribute("class", 'fri');
+divFri.setAttribute('contenteditable', 'true');
+divFri.classList="Rtable-cell row_data";
+ content.appendChild(divSat);
+divSat.setAttribute("edit_type", "click");
+divSat.setAttribute("class", 'sat');
+divSat.setAttribute('contenteditable', 'true');
+divSat.classList="Rtable-cell row_data";
+ content.appendChild(divTotalPieces);
+divTotalPieces.setAttribute("edit_type", "click");
+divTotalPieces.setAttribute("class", 'piece-total');
+divTotalPieces.setAttribute('contenteditable', 'true');
+divTotalPieces.classList="Rtable-cell row_data";
+ content.appendChild(divRate);
+divRate.setAttribute("edit_type", "click");
+divRate.setAttribute("class", 'rate');
+divRate.setAttribute('contenteditable', 'true');
+divRate.classList="Rtable-cell row_data";
+divRate.appendChild(employeeRate);
+ content.appendChild(divNetTotal);
+divNetTotal.setAttribute("edit_type", "click");
+divNetTotal.setAttribute("class", 'netTotal');
+divNetTotal.setAttribute('contenteditable', 'true');
+divNetTotal.classList="Rtable-cell row_data";
+
+idd.appendChild(employeeIdNow);
+fName.appendChild(employeeName);
+divRate.appendChild(employeeRate);
+//   let idd = document.getElementsByName('employeeId')[0];
+//
+//   idd.appendChild(employeeIdNow);
+//   let names = document.getElementsByName('name')[0];
+//     names.append(employeeName);
+// document.getElementsByName('rate')[0].appendChild(employeeRate);
+
+    // $('rate')[0].appendChild(employeeRate);
 
 
-  content.appendChild(button);
-  for (j = 0; j < 12; j++) {
-
-    let div = document.createElement('div');
-    let array = ['employeeId','name','date','date','date','date','date','date','date','total','rate','nettotal'];
-    div.setAttribute("edit_type", "click");
-    div.setAttribute('contenteditable', 'false');
-    div.setAttribute("id", array[j]);
-    div.setAttribute("value", 'ape');
-    div.classList = "Rtable-cell row_data"
-    content.appendChild(div);
-  }
-
-  //same as get getElementById
- $('#employeeId')[0].append(employeeIdNow);
- $('#name')[0].append(employeeName);
- $('#rate')[0].append(employeeRate);
-$('date')[0].
 
 
   hide_form(id);
   hide_pieceWork(id);
   console.log(content, "inside");
-})
-console.log(content, 'outside');
 
-//
-// buttons.addEventListener('click', function(){
-//
-// })
+});
+
+function createEmployeeDivs(){
+
+}
+
+
+
 $(document).on('click', '#buttons', function() {
   let id;
-  $(this).attr("aria-selected", $(this).attr("aria-selected") !== "false");
+  // $(this).attr("aria-selected", $(this).attr("aria-selected") !== "false");
   show_pieceWork(id);
+
+
+console.log('WORKING');
+
   // $(this).on("click", "#.Accordion", function() {
   //   $(this).attr("aria-selected", $(this).attr("aria-selected") == "true" );
   //   show_pieceWork(id);
   // });
 
 });
-function currentDate (){
+  console.log($(" .js-RtableAccordions"));
+function currentDate(month, day, year) {
   var todayTime = new Date();
-    var month = todayTime.getMonth() + 1;
-    var day = todayTime.getDate();
-    var year = todayTime.getYear();
-    return month + "/" + day + "/" + year;
+  var month = todayTime.getMonth() + 1;
+  var day = todayTime.getDate();
+  var year = todayTime.getYear();
+  return month + "/" + day + "/" + year;
 }
 document.getElementById('dateInput').value = currentDate();
+
+
 
 addPieceWorkButton.addEventListener('click', function() {
   employee.addPieceWork(parseInt(piece.value));
@@ -192,21 +327,54 @@ addPieceWorkButton.addEventListener('click', function() {
   // let div = document.createElement('div');
   // div.classList = "Rtable-cell";
 
-  // document.createElement('div');
-  // div.innerHTML = employee.pieceWork;
-  // div.innerHTML = piece.value;
-  // content.appendChild(div);
-content.firstElementChild.innerHTML = Date();
-  console.log('div');
+let employeePiece = document.createTextNode(piece.value);
+// let employeePiece = piece.value;
+console.log(employeePiece);
+  placeDateOnAppropiateCell(employeePiece);
 
-// content.firstElementChild.innerHTML = piece.value;
-  // date.appendChild(div[1]);
-  // div.appendChild(employee.employeePieceWorkTotal);
   console.log(employee.employeePieceWorkTotal(), 'piece wwork total');
   console.log(employee.netPay(), 'netpay');
   piece.value = "";
 });
+// let thursday = $('.thurs')[0];
 
+
+function placeDateOnAppropiateCell(employeePiece) {
+  let sunday = $('div[name=sun]');
+  let monday = $('#mon')[0];
+  let tuesday = $('#tues')[0];
+  let wednesday = $('#wed')[0];
+  let thursday = document.getElementsByName('thurs');
+ console.log(thursday);
+  let friday = $('#fri')[0];
+  let currentDay = new Date();
+  let day = currentDay.getDay();
+  switch (day) {
+    case 0:
+      sunday.appendChild(employeePiece);
+      break;
+    case 1:
+      break;
+      monday.appendChild(employeePiece);
+    case 2:
+      tuesday.appendChild(employeePiece);
+      break;
+    case 3:
+      wednesday.innerHTM = employeePiece;
+      break;
+    case 4:
+      thursday.appendChild(employeePiece);
+      break;
+    case 5:
+      friday.appendChild(employeePiece);
+      break;
+    case 6:
+      saturday.appendChild(employeePiece);
+      break;
+    default:
+      console.log('cant find');
+  }
+}
 
 
 console.log(crewPieceWork, 'array of the crews piece work ');
@@ -270,41 +438,44 @@ function show_form(id) {
 
 
 
-$(document).ready(function($) {
-  "use strict";
-  $.fn.responsiveTable = function() {
+// $(document).ready(function($) {
+//   "use strict";
+//   $.fn.responsiveTable = function() {
+//
+//     var toggleColumns = function($table) {
+//       var selectedControls = [];
+//       $table.find(".Accordion, ").each(function() {
+//         selectedControls.push($(this).attr("aria-selected"));
+//       });
+//       var cellCount = 0,
+//         colCount = 0;
+//       var setNum = $table.find(".Rtable-cell").length / Math.max( $table.find(".Accordion").length);
+//       $table.find(".Rtable-cell").each(function() {
+//         $(this).addClass("hiddenSmall");
+//         if (selectedControls[colCount] === "true") $(this).removeClass("hiddenSmall");
+//         cellCount++;
+//         if (cellCount % setNum === 0) colCount++;
+//       });
+//     };
+//     $(this).each(function() {
+//       toggleColumns($(this));
+//     });
+//
+//
+//     $(this).on("click", "#.Accordion", function() {
+//       $(this).attr("aria-selected", $(this).attr("aria-selected") !== "true");
+//       toggleColumns($(this).parents(".Rtable"));
+//     });
+//
+//   };
+// }(jQuery));
+//
+// $(" .js-RtableAccordions").responsiveTable();
 
-    var toggleColumns = function($table) {
-      var selectedControls = [];
-      $table.find(".Accordion, .Tab").each(function() {
-        selectedControls.push($(this).attr("aria-selected"));
-      });
-      var cellCount = 0,
-        colCount = 0;
-      var setNum = $table.find(".Rtable-cell").length / Math.max($table.find(".Tab").length, $table.find(".Accordion").length);
-      $table.find(".Rtable-cell").each(function() {
-        $(this).addClass("hiddenSmall");
-        if (selectedControls[colCount] === "true") $(this).removeClass("hiddenSmall");
-        cellCount++;
-        if (cellCount % setNum === 0) colCount++;
-      });
-    };
-    $(this).each(function() {
-      toggleColumns($(this));
-    });
 
-    $(this).find(".Tab").click(function() {
-      $(this).attr("aria-selected", "false").siblings().attr("aria-selected", "false");
-      toggleColumns($(this).parents(".Rtable"));
-    });
 
-    $(this).on("click", "#.Accordion", function() {
-      $(this).attr("aria-selected", $(this).attr("aria-selected") !== "true");
-      toggleColumns($(this).parents(".Rtable"));
-    });
-
-  };
+let row = document.querySelectorAll('tr');
+console.log(row);
 }(jQuery));
 
-
-$(".js-RtableTabs, .js-RtableAccordions").responsiveTable();
+$(" .js-RtableAccordions").responsiveTable();
